069c6a65bdac2abd859ae65892273f374d63fda3
Michael Frye <michael@michaelfrye.me>
2013-07-12 23:11:12 +0000

    Changed web.js to output index.html

--- web.js
+++ web.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var fs = require('fs');
+
+var FILE_DEFAULT = "index.html";
 
 var app = express.createServer(express.logger());
 
+var message = function(file) {
+  file = file || FILE_DEFAULT;
+  var buf = fs.readFileSync(file);
+  return (buf.ToString('utf-8');
+};
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(message());
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
